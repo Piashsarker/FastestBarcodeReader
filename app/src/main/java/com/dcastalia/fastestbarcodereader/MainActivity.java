75ad091aaef1407ec94101908687bdf8a49cc7d1
Piashsarker <sarkerpt@gmail.com>
2017-11-21 13:06:19 +0600

    Added permission for android version=>6.0

--- app/src/main/java/com/dcastalia/fastestbarcodereader/MainActivity.java
+++ app/src/main/java/com/dcastalia/fastestbarcodereader/MainActivity.java
@@ -1,8 +1,15 @@
 package com.dcastalia.fastestbarcodereader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
     public static final String BARCODE_KEY = "BARCODE";
     private Barcode barcodeResult;
     private TextView result;
-    FloatingActionButton fab ;
+    private  FloatingActionButton fab ;
+    private final String TAG = MainActivity.class.getSimpleName() ;
+    private final int MY_PERMISSION_REQUEST_CAMERA = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +39,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initializeViews();
 
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startScan();
+                checkPermission();
             }
         });
         if(savedInstanceState != null){
@@ -42,6 +54,31 @@ public class MainActivity extends AppCompatActivity {
                 result.setText(restoredBarcode.rawValue);
                 barcodeResult = restoredBarcode;
             }
+        }
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG , getResources().getString(R.string.camera_permission_granted));
+             startScan();
+        } else {
+            requestCameraPermission();
+
+        }
+    }
+
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.camera_permission_request),
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override public void onClick(View view) {
+                    ActivityCompat.requestPermissions(MainActivity.this,  new String[] {Manifest.permission.CAMERA}, MY_PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+        } else{
+            Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.camera_permission_request),
+                    Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.CAMERA}, MY_PERMISSION_REQUEST_CAMERA);
         }
     }
 
@@ -66,6 +103,23 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         materialBarcodeScanner.startScan();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode==MY_PERMISSION_REQUEST_CAMERA && grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.thanks_for_permission), Snackbar.LENGTH_SHORT).show();
+            startScan();
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.sorry_for_not_permission), Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+
+    }
+
+
+
 
     private void initializeViews() {
         result = (TextView) findViewById(R.id.barcodeResult);
